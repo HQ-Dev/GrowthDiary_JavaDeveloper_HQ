dce9377ba93ae349fecb3db090253ceb76512a41
HQ今日磨墨 <HQ_Dev@163.com>
2015-12-27 03:36:44 +0800

    while循环

--- Java试验自留地/第三章Java的基本程序设计结构/src/第三章Java的基本程序设计结构/控制流程.java
+++ Java试验自留地/第三章Java的基本程序设计结构/src/第三章Java的基本程序设计结构/控制流程.java
@@ -1,10 +1,41 @@
 package 第三章Java的基本程序设计结构;
 
+import java.util.Scanner;
+
+/**
+ * This program demonstrates a <code>while</code> loop.
+ */
 public class 控制流程 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// while 计算需要多长时间能够存储一定数量的退休金，假定每年存入相同数量的金额
+		// 而且利率是固定。
+		// read input
+//		Scanner in = new Scanner(System.in);
+//		System.out.println("How much money do you need to retire?");
+//		double goal = in.nextDouble();
+//		System.out.println("How much money will you contribute every year?");
+//		double payment = in.nextDouble();
+//		System.out.println("Interest rate in %: ");
+//		double interestRate = in.nextDouble();
+//		
+//		double balance = 0;
+//		int year = 0;
+//		
+//		// update account balance while goal isn't reached
+//		while (balance < goal) 
+//		{
+//			// add this year's payment and interest
+//			balance += payment;
+//			double interest = balance * interestRate / 100;
+//			balance += interest;
+//			year++;
+//		}
+//		
+//		System.out.println("You can retire in "+year+" years.");
+		
+		// do while
 	}
 
 }
